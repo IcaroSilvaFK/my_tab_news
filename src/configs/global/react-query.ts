b3cafde58e2edb7df20fe779b801847bb51adc29
whyy__ <icarovsilva1@gmail.com>
2022-12-02 22:50:11 -0300

    fix: remove logs

--- src/configs/global/react-query.ts
+++ src/configs/global/react-query.ts
@@ -1,15 +1,3 @@
 import { QueryClient } from '@tanstack/react-query'
 
-export const queryClient = new QueryClient({
-  logger: {
-    log: (message) => {
-      console.log(message)
-    },
-    warn: (warn) => {
-      console.warn(warn)
-    },
-    error: (error) => {
-      console.error(error)
-    },
-  },
-})
+export const queryClient = new QueryClient()
